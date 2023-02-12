78994759730b97a77dfad4a33ba4c3fbc3a85aa5
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-02-12 16:29:07 -0300

    change sizes

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -59,21 +59,12 @@ function Pokemons() {
     e.preventDefault();
     if(pokemonName.toLowerCase() === POKEMONS[pokemonMatch - 1]){
       setViewPokemon(true);
-      return setWin(true);
-    }else{
-      setAttempts(attempts + 1)
-      console.log(attempts)
-      if(attempts === 3){
-        setClues([...clues, `The name starts with ${POKEMONS[pokemonMatch -1][0]}`])
-      }
-      if(attempts === 6){
-        setClues([...clues, `The name ends with ${POKEMONS[pokemonMatch -1][POKEMONS[pokemonMatch -1].length -1]}`])
-      }
-    return  toast('Error, try again', {
+      setWin(true);
+      return  toast('You are the winner!! Try again', {
         duration: 4000,
         position: 'top-center',
         // Custom Icon
-        icon: '❌',
+        icon: '🏆',
         // Change colors of success/error/loading icon
         iconTheme: {
           primary: '#000',
@@ -84,6 +75,33 @@ function Pokemons() {
         ariaProps: {
           role: 'status',
           'aria-live': 'polite',
+        },
+      });
+    }else{
+      setAttempts(attempts + 1)
+      console.log(attempts)
+      if(attempts === 3){
+        setClues([...clues, `The name starts with ${POKEMONS[pokemonMatch -1][0]}`])
+      }
+      if(attempts === 6){
+        setClues([...clues, `The name ends with ${POKEMONS[pokemonMatch -1][POKEMONS[pokemonMatch -1].length -1]}`])
+      }
+    return  toast('Error, try again', {
+        duration: 1000,
+        position: 'top-center',
+        // Custom Icon
+        icon: '❌',
+        // Change colors of success/error/loading icon
+        iconTheme: {
+          primary: '#000',
+          secondary: '#fff',
+        },
+        
+        // Aria
+        ariaProps: {
+          role: 'status',
+          'aria-live': 'polite',
+          
         },
       });
     }
@@ -102,20 +120,25 @@ function Pokemons() {
           )
         } 
      {
-      win ? <button className="btn_tryagain" onClick={()=> window.location.reload()}>You are the winner! Do you want to try again?</button> :
+      win ? <button className="btn_tryagain" onClick={()=> window.location.reload()}>TRY AGAIN!</button> :
       <form className="cont_form"  onSubmit={(e) => onSubmit(e)}>
       <input autoFocus onChange={(e)=> setPokemonName(e.target.value)} name="pokemon" type="text" />
       <button disabled={pokemonName.length > 0 ? false :true} type="submit">Guess</button>
     </form>
      } 
      <Toaster />
-     <div className="div_clues">
+     {
+      !win && ( 
+      <div className="div_clues">
       <div className="div_clues-col">
         {clues.map((e, i) =>{
           return <Clues key={i} message={e} />
         })}
           </div>
         </div>
+      )
+     }
+    
       {attempts > 8 && (
         <div className="cont-btn-surrender">
         <button className="btn_surrender" onClick={() => setViewPokemon(true)}>Surrender</button>
